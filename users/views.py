01b4c807adf0d065e14c3c33c4f88ce02b60ef1b
Francsco Garcia <packo.ive@gmail.com>
2024-09-20 22:30:58 -0600

    feat(02_movies): Endpoint para obtener peliculas de acuerdo a su rol

--- users/views.py
+++ users/views.py
@@ -9,6 +9,11 @@ from .serializers import UserSerializer
 # restframework
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+# python
+import requests
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -21,4 +26,22 @@ class UserViewSet(viewsets.ModelViewSet):
         elif self.action in ['destroy', 'update', 'partial_update']:
             return [IsAdminUser()]
         return super().get_permissions()
+    
+    @action(detail=False, methods=['get'], url_path='studio-ghibli')
+    def studio_ghibli(self, request):
+        user = request.user
+        role_to_endpoint = {
+            'films': 'films',
+            'people': 'people',
+            'locations': 'locations',
+            'species': 'species',
+            'vehicles': 'vehicles',
+        }
+
+        if user.role not in role_to_endpoint:
+            return Response({"error": "Role not authorized for Studio Ghibli API"}, status=403)
+
+        endpoint = role_to_endpoint[user.role]
+        response = requests.get(f'https://ghibliapi.vercel.app/{endpoint}')
+        return Response(response.json())
 
