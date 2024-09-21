fe6525e089d00988375b996f6a5613a9deac031a
Francsco Garcia <packo.ive@gmail.com>
2024-09-20 22:42:05 -0600

    Comentarios de archivos

--- users/models.py
+++ users/models.py
@@ -1,3 +1,7 @@
+"""
+Base user model
+"""
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,7 @@
+"""
+User serializers
+"""
+
 from rest_framework import serializers
 from .models import User
 
