13982bf5741627ed49f6e9c781e963102dcd8dd3
Francsco Garcia <packo.ive@gmail.com>
2024-09-23 14:48:50 -0600

    entornos de desarrollo

--- config/settings/development.py
+++ config/settings/development.py
@@ -10,4 +10,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "../", "mediafiles")
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "../", "staticfiles")
 
-DATABASES["default"] = dj_database_url.parse("postgresql://banpay_django_render_user:W6KHzbOnd3tM7QKGXyFOTrz9psH0Y2yu@dpg-crnp8me8ii6s73eud0r0-a.oregon-postgres.render.com/banpay_django_render")
+#DATABASES["default"] = dj_database_url.parse("postgresql://banpay_django_render_user:W6KHzbOnd3tM7QKGXyFOTrz9psH0Y2yu@dpg-crnp8me8ii6s73eud0r0-a.oregon-postgres.render.com/banpay_django_render")
